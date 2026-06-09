496c2d18695ac69e3ea797a24d5743f5d7b5b9bb
pi3sl1ce <57503652+pi3sl1ce@users.noreply.github.com>
2026-06-09 23:05:10 +0300

    Init commit

---
+++ StreamingClient/src/main/java/streaming/streamingclient/StreamingClient.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package streaming.streamingclient;
+
+/**
+ *
+ * @author Nevena
+ */
+public class StreamingClient {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ StreamingLoadBalancer/src/main/java/streaming/streamingloadbalancer/StreamingLoadBalancer.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package streaming.streamingloadbalancer;
+
+/**
+ *
+ * @author Nevena
+ */
+public class StreamingLoadBalancer {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
